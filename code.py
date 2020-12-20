b2161ad5ad3181689b1b68d51c12004bf03838ab
Peter McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2020-12-19 19:41:55 -0600

    Adding quote scrolling code
    Disconnected from internet currently, displaying static quotes.

--- code.py
+++ code.py
@@ -1,111 +1,74 @@
+# Quote board matrix display
+# uses AdafruitIO to serve up a quote text feed and color feed
+# random quotes are displayed, updates periodically to look for new quotes
+# avoids repeating the same quote twice in a row
+ 
 import time
+import random
 import board
+import terminalio
 from adafruit_matrixportal.matrixportal import MatrixPortal
-
-EVENT_YEAR = 2021
-EVENT_MONTH = 10
-EVENT_DAY = 31
-EVENT_HOUR = 17
-EVENT_MINUTE = 0
-
-FRAME_DURATION = 3
-FRAMES = (
-    "bmps/jack.bmp",
-    "DAYS",
-    "bmps/jetty-reduced64x32-processed.bmp",
-    "HOURS",
-    "bmps/bats.bmp",
-    "MINUTES",
-    "bmps/skull.bmp",
-    "bmps/halloween.bmp",
-)
-
-EVENT_DAY_IMAGE = "bmps/happy_halloween.bmp"
-SYNCHRONIZE_CLOCK = True
-
+ 
 # --- Display setup ---
 matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, debug=True)
-
-current_frame = None
-
+ 
 # Create a new label with the color and text selected
 matrixportal.add_text(
-    text_font="fonts/Arial-12.bdf",
-    text_position=(4, (matrixportal.graphics.display.height // 2) - 1),
-    text_color=0xEF7F31,
+    text_font=terminalio.FONT,
+    text_position=(0, (matrixportal.graphics.display.height // 2) - 1),
+    scrolling=True,
 )
-
-
-def set_time_until(unit=None):
-    event_time = time.struct_time(
-        (
-            EVENT_YEAR,
-            EVENT_MONTH,
-            EVENT_DAY,
-            EVENT_HOUR,
-            EVENT_MINUTE,
-            0,  # we don't track seconds
-            -1,
-            -1,
-            False,
-        )
-    )
-    remaining = time.mktime(event_time) - time.mktime(time.localtime())
-    if remaining <= 0:
-        # oh, its event time!
-        matrixportal.set_background(EVENT_DAY_IMAGE)
-        return
-    remaining //= 60
-    mins_remaining = remaining % 60
-    remaining //= 60
-    hours_remaining = remaining % 24
-    remaining //= 24
-    days_remaining = remaining
-
-    if unit == "DAYS":
-        text = "{} day".format(days_remaining)
-        if days_remaining != 1:
-            text += "s"
-    if unit == "HOURS":
-        text = "{} hour".format(hours_remaining)
-        if hours_remaining != 1:
-            text += "s"
-    if unit == "MINUTES":
-        text = "{} min".format(mins_remaining)
-        if mins_remaining != 1:
-            text += "s"
-    matrixportal.set_text(text)
-    matrixportal.set_background(0)
-
-
-def set_next_frame():
-    # pylint: disable=global-statement
-    global current_frame
-
-    # Advance to next frame if we already have one
-    if current_frame is not None:
-        current_frame += 1
-
-    # Loop back or set initial frame
-    if current_frame is None or current_frame >= len(FRAMES):
-        current_frame = 0
-
-    # Check if Picture or Text
-    print(FRAMES[current_frame])
-    if FRAMES[current_frame][-4:] == ".bmp":
-        matrixportal.set_background(FRAMES[current_frame])
-        matrixportal.set_text("")
-    else:
-        set_time_until(FRAMES[current_frame])
-
-# Simulate the delay in case fetching time is fast
-set_next_frame()
-start_time = time.monotonic()
-if SYNCHRONIZE_CLOCK:
-    matrixportal.get_local_time()
-while time.monotonic() < start_time + FRAME_DURATION:
-    pass
-
+ 
+# Static 'Connecting' Text
+matrixportal.add_text(
+    text_font=terminalio.FONT,
+    text_position=(2, (matrixportal.graphics.display.height // 2) - 1),
+)
+ 
+QUOTES_FEED = "sign-quotes.signtext"
+COLORS_FEED = "sign-quotes.signcolor"
+SCROLL_DELAY = 0.02
+UPDATE_DELAY = 600
+ 
+quotes = ["Hello, there.", "This is another quote..."]
+colors = ["#FF5733", "#58D68D" ]
+last_color = None
+last_quote = None
+ 
+ 
+matrixportal.set_text("Starting", 1)
+ 
+last_update = time.monotonic()
+matrixportal.set_text(" ", 1)
+quote_index = None
+color_index = None
+ 
 while True:
-    set_next_frame()
-    time.sleep(FRAME_DURATION)
+    # Choose a random quote from quotes
+    if len(quotes) > 1 and last_quote is not None:
+        while quote_index == last_quote:
+            quote_index = random.randrange(0, len(quotes))
+    else:
+        quote_index = random.randrange(0, len(quotes))
+    last_quote = quote_index
+ 
+    # Choose a random color from colors
+    if len(colors) > 1 and last_color is not None:
+        while color_index == last_color:
+            color_index = random.randrange(0, len(colors))
+    else:
+        color_index = random.randrange(0, len(colors))
+    last_color = color_index
+ 
+    # Set the quote text
+    matrixportal.set_text(quotes[quote_index])
+ 
+    # Set the text color
+    matrixportal.set_text_color(colors[color_index])
+ 
+    # Scroll it
+    matrixportal.scroll_text(SCROLL_DELAY)
+ 
+    if time.monotonic() > last_update + UPDATE_DELAY:
+        update_data()
+        last_update = time.monotonic()
